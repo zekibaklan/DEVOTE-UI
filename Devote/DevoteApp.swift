0e72fbc5e2d6e2d5918afa6d690ea1b8556924fc
Zeki Baklan <113357432+zekibaklan@users.noreply.github.com>
2023-09-12 14:30:12 +0300

    Initial Commit

---
+++ Devote/DevoteApp.swift
@@ -0,0 +1,20 @@
+//
+//  DevoteApp.swift
+//  Devote
+//
+//  Created by Zeki Baklan on 12.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct DevoteApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
